61157e194f9fceafe028ea101a43a7a9ed83d766
Gumbare <colorfuldesire@gmail.com>
2018-05-04 14:08:05 +0200

    updated table creating file 'TableCreator'

--- src/pl/coderslab/models/TableCreator.java
+++ src/pl/coderslab/models/TableCreator.java
@@ -8,22 +8,22 @@ import java.sql.Statement;
 public class TableCreator {
 
 	// creating tables in previously prepared 'school' data base
-	private static final String query1 = "CREATE TABLE user_group(id int(11) AUTO_INCREMENT,\n"
+	private static final String query1 = "CREATE TABLE UserGroup(id int(11) AUTO_INCREMENT,\n"
 			+ "                   name varchar(255),\n" + "                   PRIMARY KEY(id));";
-	private static final String query2 = "CREATE TABLE users(id bigint(20) AUTO_INCREMENT,\n"
+	private static final String query2 = "CREATE TABLE User(id bigint(20) AUTO_INCREMENT,\n"
 			+ "                   username varchar(255),\n" + "                   email varchar(255) UNIQUE,\n"
-			+ "                   password varchar(245),\n" + "                   user_group_id int(11),\n"
+			+ "                   password varchar(245),\n" + "                   UserGroup_id int(11),\n"
 			+ "                   PRIMARY KEY(id),\n"
-			+ "                   FOREIGN KEY(user_group_id) REFERENCES user_group(id) ON DELETE CASCADE);";
-	private static final String query3 = "CREATE TABLE exercise(id int(11) AUTO_INCREMENT,\n"
+			+ "                   FOREIGN KEY(UserGroup_id) REFERENCES UserGroup(id) ON DELETE CASCADE);";
+	private static final String query3 = "CREATE TABLE Exercise(id int(11) AUTO_INCREMENT,\n"
 			+ "                      title varchar(255),\n" + "                      description TEXT,\n"
 			+ "                      PRIMARY KEY(id));";
-	private static final String query4 = "CREATE TABLE solution(id int(11) AUTO_INCREMENT,\n"
+	private static final String query4 = "CREATE TABLE Solution(id int(11) AUTO_INCREMENT,\n"
 			+ "                      created DATETIME,\n" + "                      updated DATETIME,\n"
-			+ "                      description TEXT,\n" + "                      exercise_id int(11),\n"
-			+ "                      users_id bigint(20),\n" + "                      PRIMARY KEY(id),\n"
-			+ "                      FOREIGN KEY(exercise_id) REFERENCES exercise(id) ON DELETE CASCADE,\n"
-			+ "                      FOREIGN KEY(users_id) REFERENCES users(id) ON DELETE CASCADE);";
+			+ "                      description TEXT,\n" + "                      Exercise_id int(11),\n"
+			+ "                      User_id bigint(20),\n" + "                      PRIMARY KEY(id),\n"
+			+ "                      FOREIGN KEY(Exercise_id) REFERENCES Exercise(id) ON DELETE CASCADE,\n"
+			+ "                      FOREIGN KEY(User_id) REFERENCES User(id) ON DELETE CASCADE);";
 
 	public static void main(String[] args) {
 		// creating Connection
